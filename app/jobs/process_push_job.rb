0ea5336286f3f9c9ebf0028ba45962554418d13c
Adam Gyulavari <gyulavari.adam@gmail.com>
2016-12-05 10:07:42 +0100

    fix output on failures

--- app/jobs/process_push_job.rb
+++ app/jobs/process_push_job.rb
@@ -3,11 +3,11 @@ class ProcessPushJob < ApplicationJob
 
   def perform(username)
     FileUtils.rm_rf Rails.application.secrets.tmp_directory + '/zerda-java-basics'
-    system("cd #{Rails.application.secrets.tmp_directory} && git clone git@github.com:#{username}/zerda-java-basics.git")
+    output = `cd #{Rails.application.secrets.tmp_directory} && git clone git@github.com:#{username}/zerda-java-basics.git`
     FileUtils.rm_rf Rails.application.secrets.source_directory + '/com'
     FileUtils.cp_r(Rails.application.secrets.tmp_directory + '/zerda-java-basics/src/com', Rails.application.secrets.source_directory)
-    system("cd #{Rails.application.secrets.run_directory} && ./gradlew test")
-    messages = ''
+    output += `cd #{Rails.application.secrets.run_directory} && ./gradlew test`
+    messages = output
     failures = ''
     test_count = 0
     fail_count = 0
